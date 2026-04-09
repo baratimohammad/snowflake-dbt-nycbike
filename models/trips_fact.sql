8c1b848135704f07b1c8f79853e996b0317920c6
Max <maximobarati@gmail.com>
2026-04-09 10:53:26 +0000

    trips_fact table created

---
+++ models/trips_fact.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='table') }}
+
+with trips as (
+    select
+    replace(ID, '"', '') as ID,
+    replace(type, '"', '') as TYPE,
+    date(to_timestamp(replace(STARTED_AT, '"', ''))) as date,
+    replace(START_STATION_ID, '"', '') as START_STATION_ID,
+    replace(END_STATION_ID, '"', '') as END_STATION_ID,
+    replace(MEMBERSHIP, '"', '') as MEMBERSHIP,
+    timestampdiff(second, to_timestamp(replace(STARTED_AT, '"', '')), to_timestamp(replace(ENDED_AT, '"', ''))) as trip_duration
+
+
+    from
+    {{ source('demo_source', 'BIKE_TABLE') }}
+)
+select * from trips
